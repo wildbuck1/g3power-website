11ecbed502a7682a46b3a32088839efcde34a326
WILDBUCK1 <HRAUL70@GMAIL.COM>
2026-03-19 02:26:38 -0700

    Test nodemailer load

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.handler = async () => {
-  return { statusCode: 200, body: 'pong' };
+  return { statusCode: 200, body: 'nodemailer loaded: ' + !!nodemailer };
 };
